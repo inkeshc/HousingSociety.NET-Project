e6ea68ee9c259c7bd0dc2ff742b93cd9c5254769
46992_inkesh <inkesh20@gmail.com>
2021-05-14 14:45:32 +0530

    Added housing Society Management project in dot Net Framework using MVC Model

---
+++ Controllers/Society_AddController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using HousingSociety.Models;
+
+namespace HousingSociety.Controllers
+{
+    public class Society_AddController : Controller
+    {
+        private Society_AddEnitites db = new Society_AddEnitites();
+
+        // GET: Society_Add
+        public ActionResult Index()
+        {
+            return View(db.Society_Add.ToList());
+        }
+
+        // GET: Society_Add/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Society_Add society_Add = db.Society_Add.Find(id);
+            if (society_Add == null)
+            {
+                return HttpNotFound();
+            }
+            return View(society_Add);
+        }
+
+        // GET: Society_Add/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Society_Add/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "REGID,POST,Fname,Lname,Wing,Flat_No,Time_From,Time_To,Email,Password")] Society_Add society_Add)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Society_Add.Add(society_Add);
+                db.SaveChanges();
+                //return RedirectToAction("Index");
+                // ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Society Register Successfully')", true);
+                
+                return RedirectToAction("Admin_Home", "Login_Admin");
+            }
+
+            return View(society_Add);
+        }
+
+        // GET: Society_Add/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Society_Add society_Add = db.Society_Add.Find(id);
+            if (society_Add == null)
+            {
+                return HttpNotFound();
+            }
+            return View(society_Add);
+        }
+
+        // POST: Society_Add/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "REGID,POST,Fname,Lname,Wing,Flat_No,Time_From,Time_To,Email,Password")] Society_Add society_Add)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(society_Add).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(society_Add);
+        }
+
+        // GET: Society_Add/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Society_Add society_Add = db.Society_Add.Find(id);
+            if (society_Add == null)
+            {
+                return HttpNotFound();
+            }
+            return View(society_Add);
+        }
+
+        // POST: Society_Add/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Society_Add society_Add = db.Society_Add.Find(id);
+            db.Society_Add.Remove(society_Add);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Controllers/User_RequestController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using HousingSociety.Models;
+
+namespace HousingSociety.Controllers
+{
+    public class User_RequestController : Controller
+    {
+        private User_RequestEntities db = new User_RequestEntities();
+
+        // GET: User_Request
+        public ActionResult Index()
+        {
+            return View(db.User_Request.ToList());
+        }
+
+        // GET: User_Request/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User_Request user_Request = db.User_Request.Find(id);
+            if (user_Request == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user_Request);
+        }
+
+        // GET: User_Request/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User_Request/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "REGID,Fname,Lname,Wing,Flat_No,Request_Priority,Request_Detail")] User_Request user_Request)
+        {
+            if (ModelState.IsValid)
+            {
+                db.User_Request.Add(user_Request);
+                db.SaveChanges();
+                //return RedirectToAction("Index");              
+                return RedirectToAction("User_Home", "Login_User");
+            }
+
+            return View(user_Request);
+        }
+
+        // GET: User_Request/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User_Request user_Request = db.User_Request.Find(id);
+            if (user_Request == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user_Request);
+        }
+
+        // POST: User_Request/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "REGID,Fname,Lname,Wing,Flat_No,Request_Priority,Request_Detail")] User_Request user_Request)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user_Request).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(user_Request);
+        }
+
+        // GET: User_Request/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            User_Request user_Request = db.User_Request.Find(id);
+            if (user_Request == null)
+            {
+                return HttpNotFound();
+            }
+            return View(user_Request);
+        }
+
+        // POST: User_Request/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            User_Request user_Request = db.User_Request.Find(id);
+            db.User_Request.Remove(user_Request);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
